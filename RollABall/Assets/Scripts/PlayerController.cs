7bdfc9935592968cb98b3ceb1a8e3dff773f87b7
matsalen <k.kurosaki@hotmail.com>
2019-05-07 17:12:13 -0300

    Jogo base completo.

---
+++ RollABall/Assets/Scripts/PlayerController.cs
@@ -0,0 +1,39 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class PlayerController : MonoBehaviour
+{
+
+    [SerializeField] private float velocidade;
+    private float movimentoH;
+    private float movimentoV;
+    private Vector3 movimento;
+    private Rigidbody rb;
+    
+    // Start is called before the first frame update
+    void Start()
+    {
+        rb = GetComponent<Rigidbody>();
+    }
+
+    // Update is called once per frame
+    void Update()
+    {
+        movimentoH = Input.GetAxis("Horizontal");
+        movimentoV = Input.GetAxis("Vertical");
+
+        movimento = new Vector3(movimentoH, 0, movimentoV);
+        rb.AddForce(movimento * velocidade);
+
+
+    }
+
+    private void OnTriggerEnter(Collider other)
+    {
+        if (other.gameObject.CompareTag("PowerUp"))
+        {
+            other.gameObject.SetActive(false);
+        }
+    }
+}
